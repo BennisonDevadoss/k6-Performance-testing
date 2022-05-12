abed8e0fd687485e91cc654a0a9d34d97d301ae9
bennisondevadoss <bennisondevadoss@gmail.com>
2022-05-12 10:28:46 +0530

    Learning K6 options

--- http_get.mjs
+++ http_get.mjs
@@ -1,5 +1,11 @@
 import http from 'k6/http';
 
+export const options = {
+    linger: true,   // linger  is giving the error 
+    // vus: 2,
+    // // iterations: 100,
+    // duration: '30s',
+}
 export default function () {
     http.get(`http://test.k6.io`);
 }
